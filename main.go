a3fc0af9e72658a999a49172de9ddcee5c5335a4
kenju <ken901waga@gmail.com>
2018-05-14 23:21:21 +0900

    chdir and exec command

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "os"
+  "os/exec"
   "path/filepath"
 )
 
@@ -18,26 +19,40 @@ func main() {
   }
 
   // TODO: change this O(N^3) for loop with goroutine
+  // ex. $GOPATH/src/github.com/
   for _, host := range matches {
     users, err := filepath.Glob(host+"/*")
     if err != nil {
       fmt.Printf("error file globbing: %+v\n", err)
     }
 
+    // ex. $GOPATH/src/github.com/kenju
     for _, user := range users {
       repos, err := filepath.Glob(user+"/*")
       if err != nil {
         fmt.Printf("error file globbing: %+v\n", err)
       }
 
+      // ex. $GOPATH/src/github.com/kenju/go-groom
       for _, repo := range repos {
         fi, err := os.Stat(repo)
         if err != nil {
           fmt.Printf("error while getting os stat for %+v\n", fi)
         }
         if fi.IsDir() {
-          // TODO: cw and run git pull
-          fmt.Printf("%+v is a repository.\n", repo)
+          // change dir
+          fmt.Printf("[INFO] chdir %s\n", repo)
+          err := os.Chdir(repo)
+          if err != nil {
+            fmt.Printf("error while changeing directory to %s\n", repo)
+          }
+          // run command
+          out, err := exec.Command("git", "checkout", "master").Output()
+          if err != nil {
+            fmt.Printf("error while executing command at %s\n", repo)
+          }
+          fmt.Println(string(out))
+          // out, err := exec.Command("git", "pull", "--prune")
         } else {
           fmt.Printf("%+v is not a dir. skipping.\n", fi.Name())
         }
