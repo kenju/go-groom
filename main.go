aeaf9d0c862f9b80e72e92b7894dedf74d8e1bff
kenju <ken901waga@gmail.com>
2018-05-14 23:52:31 +0900

    refactor main.go by splitting methods

--- main.go
+++ main.go
@@ -3,32 +3,49 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
+	"os/exec"
 )
 
-func main() {
-	// ex. $GOPATH/src
-	dir := filepath.Join(os.Getenv("GOPATH"), "src", "*")
-	matches, err := filepath.Glob(dir)
+func runGroomCommand(repo string) {
+	// change dir
+	fmt.Printf("[INFO] chdir %s\n", repo)
+	err := os.Chdir(repo)
+	if err != nil {
+		fmt.Printf("error while changeing directory to %s\n", repo)
+	}
+	// `git checkout master`
+	out, err := exec.Command("git", "checkout", "master").Output()
+	if err != nil {
+		fmt.Printf("error while `git checkout master` at %s: %+v\n", repo, err)
+	}
+	fmt.Println(string(out))
+	// `git pull --prune`
+	out, err = exec.Command("git", "pull", "--prune").Output()
+	if err != nil {
+		fmt.Printf("error while `git pull --prune` at %s: %+v\n", repo, err)
+	}
+	fmt.Println(string(out))
+}
+
+func flattenWalk(path string) []string {
+	matches, err := filepath.Glob(filepath.Join(path, "*"))
 	if err != nil {
 		fmt.Printf("error file globbing: %+v\n", err)
 	}
+	return matches
+}
+
+func main() {
+	dir := filepath.Join(os.Getenv("GOPATH"), "src")
+	matches := flattenWalk(dir) // ex. $GOPATH/src
 
 	// TODO: change this O(N^3) for loop with goroutine
-	// ex. $GOPATH/src/github.com/
 	for _, host := range matches {
-		users, err := filepath.Glob(filepath.Join(host, "*"))
-		if err != nil {
-			fmt.Printf("error file globbing: %+v\n", err)
-		}
+		users := flattenWalk(host) // ex. $GOPATH/src/github.com/
 
-		// ex. $GOPATH/src/github.com/kenju
 		for _, user := range users {
-			repos, err := filepath.Glob(filepath.Join(user, "*"))
-			if err != nil {
-				fmt.Printf("error file globbing: %+v\n", err)
-			}
+			repos := flattenWalk(user) // ex. $GOPATH/src/github.com/kenju
 
 			// ex. $GOPATH/src/github.com/kenju/go-groom
 			for _, repo := range repos {
@@ -37,24 +54,7 @@ func main() {
 					fmt.Printf("error while getting os stat for %+v\n", fi)
 				}
 				if fi.IsDir() {
-					// change dir
-					fmt.Printf("[INFO] chdir %s\n", repo)
-					err := os.Chdir(repo)
-					if err != nil {
-						fmt.Printf("error while changeing directory to %s\n", repo)
-					}
-					// `git checkout master`
-					out, err := exec.Command("git", "checkout", "master").Output()
-					if err != nil {
-						fmt.Printf("error while `git checkout master` at %s: %+v\n", repo, err)
-					}
-					fmt.Println(string(out))
-					// `git pull --prune`
-					out, err = exec.Command("git", "pull", "--prune").Output()
-					if err != nil {
-						fmt.Printf("error while `git pull --prune` at %s: %+v\n", repo, err)
-					}
-					fmt.Println(string(out))
+					runGroomCommand(repo)
 				} else {
 					fmt.Printf("%+v is not a dir. skipping.\n", fi.Name())
 				}
