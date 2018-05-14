567c89379354f5f32b91cf97f1e8de020d8071ea
kenju <ken901waga@gmail.com>
2018-05-14 23:07:10 +0900

    recursibely walk repos

--- main.go
+++ main.go
@@ -2,8 +2,46 @@ package main
 
 import (
   "fmt"
+  "os"
+  "path/filepath"
 )
 
 func main() {
   fmt.Println("Hello, world!")
+
+  // TODO: replace with $GO_PATH
+  dir := "/Users/kenjuwagatsuma/.ghq/src/*"
+
+  matches, err := filepath.Glob(dir)
+  if err != nil {
+    fmt.Printf("error file globbing: %+v\n", err)
+  }
+
+  // TODO: change this O(N^3) for loop with goroutine
+  for _, host := range matches {
+    users, err := filepath.Glob(host+"/*")
+    if err != nil {
+      fmt.Printf("error file globbing: %+v\n", err)
+    }
+
+    for _, user := range users {
+      repos, err := filepath.Glob(user+"/*")
+      if err != nil {
+        fmt.Printf("error file globbing: %+v\n", err)
+      }
+
+      for _, repo := range repos {
+        fi, err := os.Stat(repo)
+        if err != nil {
+          fmt.Printf("error while getting os stat for %+v\n", fi)
+        }
+        if fi.IsDir() {
+          // TODO: cw and run git pull
+          fmt.Printf("%+v is a repository.\n", repo)
+        } else {
+          fmt.Printf("%+v is not a dir. skipping.\n", fi.Name())
+        }
+      }
+    }
+  }
 }
