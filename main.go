c0d462b0cf0b673dc376080209eeae0f17b0548a
kenju <ken901waga@gmail.com>
2018-05-15 01:22:26 +0900

    run command with goroutine

--- main.go
+++ main.go
@@ -2,30 +2,68 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
-	"path/filepath"
 	"os/exec"
+	"path/filepath"
+	"runtime"
+	"sync"
 )
 
+func main() {
+	recursivelyRunGroomCommand()
+	log.Printf("NumGoroutine: %d\n", runtime.NumGoroutine())
+}
+
+// TODO: get script from STDIN and execute instead
+func recursivelyRunGroomCommand() {
+	dir := filepath.Join(os.Getenv("GOPATH"), "src")
+	matches := flattenWalk(dir) // ex. $GOPATH/src
+
+	waitgroup := &sync.WaitGroup{}
+
+	for _, host := range matches { // ex. $GOPATH/src/github.com/
+		users := flattenWalk(host)
+
+		for _, user := range users { // ex. $GOPATH/src/github.com/kenju
+			repos := flattenWalk(user)
+
+			for _, repo := range repos { // ex. $GOPATH/src/github.com/kenju/go-groom
+				fi, err := os.Stat(repo)
+				if err != nil {
+					fmt.Printf("error while getting os stat for %+v\n", fi)
+				}
+				if fi.IsDir() {
+					waitgroup.Add(1)
+					go func(r string, wg *sync.WaitGroup) {
+						defer wg.Done()
+						runGroomCommand(r)
+					}(repo, waitgroup)
+				}
+			}
+		}
+	}
+
+	waitgroup.Wait()
+}
+
+func runCommand(name, dir string, arg ...string) string {
+	cmd := exec.Command(name, arg...)
+	cmd.Dir = dir
+
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("%s: %s", dir, fmt.Sprint(err))
+	}
+	return string(out)
+}
+
 func runGroomCommand(repo string) {
-	// change dir
-	fmt.Printf("[INFO] chdir %s\n", repo)
-	err := os.Chdir(repo)
-	if err != nil {
-		fmt.Printf("error while changeing directory to %s\n", repo)
-	}
-	// `git checkout master`
-	out, err := exec.Command("git", "checkout", "master").Output()
-	if err != nil {
-		fmt.Printf("error while `git checkout master` at %s: %+v\n", repo, err)
-	}
-	fmt.Println(string(out))
-	// `git pull --prune`
-	out, err = exec.Command("git", "pull", "--prune").Output()
-	if err != nil {
-		fmt.Printf("error while `git pull --prune` at %s: %+v\n", repo, err)
-	}
-	fmt.Println(string(out))
+	// TODO: update log format
+	fmt.Printf("[%s] %s", repo, runCommand("git", repo, "rev-parse", "--show-toplevel"))
+	fmt.Printf("[%s] %s", repo, runCommand("git", repo, "rev-parse", "--abbrev-ref", "HEAD"))
+	fmt.Printf("[%s] %s", repo, runCommand("git", repo, "checkout", "master"))
+	fmt.Printf("[%s] %s", repo, runCommand("git", repo, "pull", "--prune"))
 }
 
 func flattenWalk(path string) []string {
@@ -34,31 +72,4 @@ func flattenWalk(path string) []string {
 		fmt.Printf("error file globbing: %+v\n", err)
 	}
 	return matches
-}
-
-func main() {
-	dir := filepath.Join(os.Getenv("GOPATH"), "src")
-	matches := flattenWalk(dir) // ex. $GOPATH/src
-
-	// TODO: change this O(N^3) for loop with goroutine
-	for _, host := range matches {
-		users := flattenWalk(host) // ex. $GOPATH/src/github.com/
-
-		for _, user := range users {
-			repos := flattenWalk(user) // ex. $GOPATH/src/github.com/kenju
-
-			// ex. $GOPATH/src/github.com/kenju/go-groom
-			for _, repo := range repos {
-				fi, err := os.Stat(repo)
-				if err != nil {
-					fmt.Printf("error while getting os stat for %+v\n", fi)
-				}
-				if fi.IsDir() {
-					runGroomCommand(repo)
-				} else {
-					fmt.Printf("%+v is not a dir. skipping.\n", fi.Name())
-				}
-			}
-		}
-	}
 }
